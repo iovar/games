b621ff41a7526d228cc31b72d04a4b0c28d87546
Ioannis Varouchakis <johnvarouchakis@gmail.com>
2023-07-27 22:01:00 +0200

    some config, style fixes, images, card component

--- js/components/game-card/game-card.js
+++ js/components/game-card/game-card.js
@@ -2,7 +2,7 @@ import config from '../../../config.js';
 
 const { BaseComponent } = await import(`${config.LIB_DIR}/base-component.js`);
 
-export class PageTitle extends BaseComponent {
+export class GameCard extends BaseComponent {
     constructor() {
         super(import.meta.url, { dynamic: false });
     }
--- js/register-components.js
+++ js/register-components.js
@@ -1,3 +1,5 @@
+import { GameCard } from './components/game-card/game-card.js';
 import { PageTitle } from './components/page-title/page-title.js';
 
+customElements.define('game-card-x', GameCard);
 customElements.define('page-title-x', PageTitle);
