85b216912dee79218ef572f5a1da7c9b0adc1276
Ioannis Varouchakis <johnvarouchakis@gmail.com>
2023-08-06 17:18:35 +0200

    a bit of layout

--- js/register-components.js
+++ js/register-components.js
@@ -1,5 +1,7 @@
 import { GameCard } from './components/game-card/game-card.js';
+import { Layout } from './components/layout/layout.js';
 import { PageTitle } from './components/page-title/page-title.js';
 
 customElements.define('game-card-x', GameCard);
 customElements.define('page-title-x', PageTitle);
+customElements.define('layout-x', Layout);
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'cache-v3';
+const cacheName = 'cache-v4';
 
 const getCachedFiles = async () => {
     const response = await fetch('./config.json');
